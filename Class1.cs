9c98dc13ddeb7bdeed2586b57d1553d89187a368
AditiMahangade <aditi.a.mahangade-1@ou.edu>
2024-08-29 10:15:51 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace s;
+public class Student
+{
+	public Student()
+	{
+		public string yourName { get; set; }
+
+	public Student()
+	{
+		yourName = string.Empty;
+	}
+
+	public override string ToString()
+	{
+		return $"{yourName}";
+	}
+	}
+}
---
+++ FirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            windowMain.Background = Brushes.LightPink;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            submitButton.Background = Brushes.Red;
+
+
+            string birth = birthDate.Text;
+            int newBirth = Convert.ToInt32(birthDate);
+            string yourName = textName.Text;
+
+            
+
+            if (string.IsNullOrWhiteSpace(yourName) == false)
+            {
+                MessageBox.Show($"Hi! {yourName}, welcome to my program!");
+            }
+
+            if (string.IsNullOrWhiteSpace(yourName) == true)
+            {
+                MessageBox.Show($"Error. You must enter something");
+            }
+        }
+    }
+}
